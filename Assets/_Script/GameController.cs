c9673d81ba85d717eeed1f76089a241dd885b24c
vinson <vinsonleonardi@gmail.com>
2024-12-16 13:44:22 +0700

    first person camera movement

--- Assets/_Script/GameController.cs
+++ Assets/_Script/GameController.cs
@@ -3,90 +3,90 @@ using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
 {
-    public Camera playerCamera; 
-    public Camera buildCamera; 
+    public Camera playerCamera;
+    public Camera buildCamera;
     public Camera leftCamera;
     public Camera rightCamera;
     public Camera behindCamera;
-    public GameObject Panel; 
+    public GameObject Panel;
     public GameObject ButtonKhusus;
     public Button buildButton;
-    public Button saveButton; 
+    public Button saveButton;
     public Button backButton;
-    public Button removeButton; 
+    public Button removeButton;
     public Button nextButton;
     public Button previousButton;
     private bool isPlacing = false;
     private bool isBuilding = false;
 
-    public float mouseSensitivity = 50f; 
-    public float movementSpeed = 10f; 
+    public float mouseSensitivity = 50f;
+    public float movementSpeed = 10f;
 
     public void Start()
-{
-    // Menonaktifkan semua elemen UI selain BuildButton
-    ButtonKhusus.SetActive(false);
-    Panel.SetActive(false);
-    saveButton.gameObject.SetActive(false);
-    backButton.gameObject.SetActive(false);
-    buildCamera.gameObject.SetActive(false);
-    leftCamera.gameObject.SetActive(false);
-    rightCamera.gameObject.SetActive(false);
-    behindCamera.gameObject.SetActive(false);
+    {
+        // Menonaktifkan semua elemen UI selain BuildButton
+        ButtonKhusus.SetActive(false);
+        Panel.SetActive(false);
+        saveButton.gameObject.SetActive(false);
+        backButton.gameObject.SetActive(false);
+        buildCamera.gameObject.SetActive(false);
+        leftCamera.gameObject.SetActive(false);
+        rightCamera.gameObject.SetActive(false);
+        behindCamera.gameObject.SetActive(false);
 
-    // Menambahkan listener untuk tombol
-    buildButton.onClick.AddListener(StartBuildMode);
-}
+        // Menambahkan listener untuk tombol
+        buildButton.onClick.AddListener(StartBuildMode);
+    }
 
     public void StartBuildMode()
-{
-    Debug.Log("StartBuildMode called.");
-    isBuilding = true;
-    isPlacing = true;
-    
-    // Alihkan ke Build Camera
-    playerCamera.gameObject.SetActive(false);
-    buildCamera.gameObject.SetActive(true);
-    rightCamera.gameObject.SetActive(false);
-    leftCamera.gameObject.SetActive(false);
-    behindCamera.gameObject.SetActive(false);
+    {
+        Debug.Log("StartBuildMode called.");
+        isBuilding = true;
+        isPlacing = true;
 
-    // Tampilkan elemen Build Mode
-    ButtonKhusus.SetActive(true);
-    Panel.SetActive(true);
-    buildButton.gameObject.SetActive(false);
-    saveButton.gameObject.SetActive(true);
-    backButton.gameObject.SetActive(true);
-    removeButton.gameObject.SetActive(true);
-    nextButton.gameObject.SetActive(true);
-    previousButton.gameObject.SetActive(true);
-}
+        // Alihkan ke Build Camera
+        playerCamera.gameObject.SetActive(false);
+        buildCamera.gameObject.SetActive(true);
+        rightCamera.gameObject.SetActive(false);
+        leftCamera.gameObject.SetActive(false);
+        behindCamera.gameObject.SetActive(false);
+
+        // Tampilkan elemen Build Mode
+        ButtonKhusus.SetActive(true);
+        Panel.SetActive(true);
+        buildButton.gameObject.SetActive(false);
+        saveButton.gameObject.SetActive(true);
+        backButton.gameObject.SetActive(true);
+        removeButton.gameObject.SetActive(true);
+        nextButton.gameObject.SetActive(true);
+        previousButton.gameObject.SetActive(true);
+    }
 
     public void EndBuildMode()
-{
-    Debug.Log("EndBuildMode called.");
-    isBuilding = false;
-    isPlacing = false;
-    
-    // Alihkan ke Build Camera
-    playerCamera.gameObject.SetActive(true);
+    {
+        Debug.Log("EndBuildMode called.");
+        isBuilding = false;
+        isPlacing = false;
 
-    buildCamera.gameObject.SetActive(false);
+        // Alihkan ke Build Camera
+        playerCamera.gameObject.SetActive(true);
 
-    // Tampilkan elemen Build Mode
-    ButtonKhusus.SetActive(false);
-    Panel.SetActive(false);
-    buildButton.gameObject.SetActive(true);
-    saveButton.gameObject.SetActive(false);
-    backButton.gameObject.SetActive(false);
-    removeButton.gameObject.SetActive(false);
-    nextButton.gameObject.SetActive(false);
-    previousButton.gameObject.SetActive(false);
+        buildCamera.gameObject.SetActive(false);
 
-    // Bebaskan kursor untuk interaksi UI
-    Cursor.lockState = CursorLockMode.None;
-    Cursor.visible = true;
-}
+        // Tampilkan elemen Build Mode
+        ButtonKhusus.SetActive(false);
+        Panel.SetActive(false);
+        buildButton.gameObject.SetActive(true);
+        saveButton.gameObject.SetActive(false);
+        backButton.gameObject.SetActive(false);
+        removeButton.gameObject.SetActive(false);
+        nextButton.gameObject.SetActive(false);
+        previousButton.gameObject.SetActive(false);
+
+        // Bebaskan kursor untuk interaksi UI
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
 
     public void Update()
     {
@@ -94,41 +94,63 @@ public class GameController : MonoBehaviour
         if (!isBuilding)
         {
             HandleCameraMovement();
-        }else
-    {
-        // Cek jika tombol Home ditekan saat dalam mode Build
-        if (Input.GetKeyDown(KeyCode.Home))
-        {
-            isBuilding = false;
-            EndBuildMode(); // Mengembalikan ke mode kamera utama
         }
+        else
+        {
+            // Cek jika tombol Home ditekan saat dalam mode Build
+            if (Input.GetKeyDown(KeyCode.Home))
+            {
+                isBuilding = false;
+                EndBuildMode(); // Mengembalikan ke mode kamera utama
+            }
 
-        if (isPlacing)
-        {
-            HandlePlacement(); // Menangani proses placement objek
+            if (isPlacing)
+            {
+                HandlePlacement(); // Menangani proses placement objek
+            }
         }
-    }
     }
 
     void HandleCameraMovement()
     {
-        // Pergerakan kamera di sumbu X (maju mundur)
-        float moveDirection = Input.GetAxis("Vertical") * movementSpeed * Time.deltaTime;
+        // Pergerakan posisi kamera
+        float horizontalMovement = Input.GetAxis("Horizontal") * movementSpeed * Time.deltaTime; // A dan D untuk kiri dan kanan
+        float verticalMovement = Input.GetAxis("Vertical") * movementSpeed * Time.deltaTime; // W dan S untuk maju dan mundur
 
-        // Menggerakkan kamera dengan menggunakan input W dan S
-        Vector3 movement = playerCamera.transform.forward * moveDirection;
-        movement.y = 0; // Menghilangkan pergerakan di sumbu Y
+        // Mendapatkan arah pergerakan berdasarkan orientasi kamera
+        Vector3 forwardMovement = playerCamera.transform.forward * verticalMovement;
+        Vector3 rightMovement = playerCamera.transform.right * horizontalMovement;
 
-        playerCamera.transform.position += movement; // Menggerakkan kamera
+        // Menambahkan pergerakan pada posisi kamera
+        Vector3 movement = forwardMovement + rightMovement;
+        movement.y = 0; // Menonaktifkan pergerakan vertikal (naik/turun)
+        playerCamera.transform.position += movement;
+
+        // Rotasi kamera dengan mouse
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * 4 * Time.deltaTime; // Gerakan mouse horizontal
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * 4 * Time.deltaTime; // Gerakan mouse vertikal
+
+        // Mengatur rotasi horizontal (yaw)
+        playerCamera.transform.Rotate(Vector3.up * mouseX, Space.World);
+
+        // Mengatur rotasi vertikal (pitch) dengan perbaikan bug
+        Vector3 currentRotation = playerCamera.transform.localEulerAngles;
+        float pitch = currentRotation.x;
+        if (pitch > 180) pitch -= 360; // Mengatasi rotasi Euler yang tiba-tiba menjadi sangat besar
+
+        float newPitch = Mathf.Clamp(pitch - mouseY, -89f, 89f); // Membatasi pitch antara -89 dan 89 derajat
+        playerCamera.transform.localEulerAngles = new Vector3(newPitch, currentRotation.y, 0);
     }
+
+
 
     void HandlePlacement()
-{
-    // Cek input untuk menempatkan objek
-    if (Input.GetMouseButtonDown(0)) // Tombol kiri mouse untuk menempatkan objek
     {
-        // Logika placement objek, bisa menambahkan objek ke dunia atau memulai proses lainnya
-        Debug.Log("Placement done!");
+        // Cek input untuk menempatkan objek
+        if (Input.GetMouseButtonDown(0)) // Tombol kiri mouse untuk menempatkan objek
+        {
+            // Logika placement objek, bisa menambahkan objek ke dunia atau memulai proses lainnya
+            Debug.Log("Placement done!");
+        }
     }
-}
 }
